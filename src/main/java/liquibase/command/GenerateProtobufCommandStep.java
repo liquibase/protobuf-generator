be923f92bf1044baa1ad654ff05cca7b1719f392
Derek Smart <derek@grindaga.com>
2022-10-07 13:35:26 -0400

    optionally write single protobuf file
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- src/main/java/liquibase/command/GenerateProtobufCommandStep.java
+++ src/main/java/liquibase/command/GenerateProtobufCommandStep.java
@@ -9,6 +9,7 @@ import liquibase.util.StringUtil;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.SortedSet;
@@ -19,12 +20,16 @@ public class GenerateProtobufCommandStep extends AbstractCommandStep {
 
     public static final String[] COMMAND_NAME = {"generateProtobuf"};
     public static final CommandArgumentDefinition<String> OUTPUT_DIR_ARG;
+    public static final CommandArgumentDefinition<String> TARGET_COMMAND;
 
     static {
         CommandBuilder builder = new CommandBuilder(GenerateProtobufCommandStep.COMMAND_NAME);
         OUTPUT_DIR_ARG = builder.argument("outputDir", String.class)
                 .defaultValue("proto")
-                .description("Director for protobuf output").build();
+                .description("Directory for protobuf output").build();
+        TARGET_COMMAND = builder.argument("targetCommand", String.class)
+                .defaultValue("")
+                .description("Individual Command to generate protobuf").build();
     }
 
     @Override
@@ -36,70 +41,78 @@ public class GenerateProtobufCommandStep extends AbstractCommandStep {
     public void run(CommandResultsBuilder resultsBuilder) throws Exception {
         CommandScope commandScope = resultsBuilder.getCommandScope();
         String outputDir = commandScope.getArgumentValue(OUTPUT_DIR_ARG);
-        for (CommandDefinition commandDefinition : getCommands()) {
+        String targetCommand = commandScope.getArgumentValue(TARGET_COMMAND);
 
-            //Don't generate protobuf for generateProtobuf command step
-            if (commandDefinition.getName() == COMMAND_NAME) {
-                continue;
-            }
-
-            String commandName = commandDefinition.getName()[0];
-            if (commandDefinition.getName().length > 1) {
-                commandName += "_" + commandDefinition.getName()[1];
-            }
-            System.out.println(commandName);
-
-            // Files should be named lower_snake_case.proto
-            // https://developers.google.com/protocol-buffers/docs/style#file_structure
-            commandName = toSnakeCase(commandName);
-            String uCommandName = StringUtil.upperCaseFirst(StringUtil.toCamelCase(commandName));
-
-            String fileName = commandName + ".proto";
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputDir + "/" + fileName))) {
-                writer.write("syntax = \"proto3\";\n");
-                writer.write("\n");
-                writer.write("option go_package=\"./;proto\";\n"); //TODO get proper golang package path
-                writer.write("option java_package = \"org.liquibase.grpc.proto\";\n");
-                writer.write("option java_multiple_files = true;\n");
-                writer.write("option java_outer_classname = \"" + uCommandName + "Proto\";\n");
-                writer.write("\n");
-                writer.write("/* " + commandDefinition.getShortDescription() + " */");
-                writer.write("\n");
-                writer.write("package " + commandName + ";\n");
-                writer.write("\n");
-                writer.write("service " + uCommandName + "Service {\n");
-                writer.write("  rpc execute(" + uCommandName + "Request) returns (Response) {}\n");
-                writer.write("}\n");
-                writer.write("\n");
-                writer.write("message " + uCommandName + "Request {\n");
-                String optional = "  optional";
-                int i=1;
-                Map<String, CommandArgumentDefinition<?>> arguments = commandDefinition.getArguments();
-                for (Map.Entry<String, CommandArgumentDefinition<?>> entry : arguments.entrySet()) {
-                    String dataTypeName = entry.getValue().getDataType().getSimpleName();
-                    String argumentName = entry.getKey();
-                    if (dataTypeName.equalsIgnoreCase("string")) {
-                        writer.write(optional + " string " + argumentName + " = " + Integer.toString(i) + ";");
-                    } else if (dataTypeName.equalsIgnoreCase("boolean")) {
-                        writer.write(optional + " bool " + argumentName + " = " + Integer.toString(i) + ";");
-                    } else if (dataTypeName.equalsIgnoreCase("Integer")) {
-                        writer.write(optional + " int32 " + argumentName + " = " + Integer.toString(i) + ";");
-                    } else {
-                        writer.write(optional + " string " + argumentName + " = " + Integer.toString(i) + ";");
-                    }
-                    String required = entry.getValue().isRequired() ? "*required* " : "";
-                    writer.write(" // " + required + entry.getValue().getDescription() + "\n");
-                    i++;
+        if (targetCommand != "") {
+            writeToFile(getCommand(targetCommand), outputDir);
+        } else {
+            // Generate protobuf for all commands
+            for (CommandDefinition commandDefinition : getCommands()) {
+                //Don't generate protobuf for generateProtobuf command step
+                if (commandDefinition.getName() == COMMAND_NAME) {
+                    continue;
                 }
-                writer.write( "  map<string, string> configuration = " + i + ";\n");
-                writer.write("}\n");
-                writer.write("\n");
-                writer.write("message Response {\n");
-                writer.write("  string message = 1;\n");
-                writer.write("}\n");
+                writeToFile(commandDefinition, outputDir);
             }
         }
-        getConfigurations();
+        // getConfigurations(); TODO doesn't look to be used.
+    }
+
+
+    private void writeToFile(CommandDefinition commandDefinition, String outputDir) throws IOException {
+        String commandName = commandDefinition.getName()[0];
+        if (commandDefinition.getName().length > 1) {
+            commandName += "_" + commandDefinition.getName()[1];
+        }
+        // Files should be named lower_snake_case.proto
+        // https://developers.google.com/protocol-buffers/docs/style#file_structure
+        commandName = toSnakeCase(commandName);
+        String uCommandName = StringUtil.upperCaseFirst(StringUtil.toCamelCase(commandName));
+        String fileName = commandName + ".proto";
+        System.out.println( "writing "  + fileName);
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputDir + "/" + fileName))) {
+            writer.write("syntax = \"proto3\";\n");
+            writer.write("\n");
+            writer.write("option go_package=\"./;proto\";\n"); //TODO get proper golang package path
+            writer.write("option java_package = \"org.liquibase.grpc.proto\";\n");
+            writer.write("option java_multiple_files = true;\n");
+            writer.write("option java_outer_classname = \"" + uCommandName + "Proto\";\n");
+            writer.write("\n");
+            writer.write("/* " + commandDefinition.getShortDescription() + " */");
+            writer.write("\n");
+            writer.write("package " + commandName + ";\n");
+            writer.write("\n");
+            writer.write("service " + uCommandName + "Service {\n");
+            writer.write("  rpc execute(" + uCommandName + "Request) returns (Response) {}\n");
+            writer.write("}\n");
+            writer.write("\n");
+            writer.write("message " + uCommandName + "Request {\n");
+            String optional = "  optional";
+            int i=1;
+            Map<String, CommandArgumentDefinition<?>> arguments = commandDefinition.getArguments();
+            for (Map.Entry<String, CommandArgumentDefinition<?>> entry : arguments.entrySet()) {
+                String dataTypeName = entry.getValue().getDataType().getSimpleName();
+                String argumentName = entry.getKey();
+                if (dataTypeName.equalsIgnoreCase("string")) {
+                    writer.write(optional + " string " + argumentName + " = " + Integer.toString(i) + ";");
+                } else if (dataTypeName.equalsIgnoreCase("boolean")) {
+                    writer.write(optional + " bool " + argumentName + " = " + Integer.toString(i) + ";");
+                } else if (dataTypeName.equalsIgnoreCase("Integer")) {
+                    writer.write(optional + " int32 " + argumentName + " = " + Integer.toString(i) + ";");
+                } else {
+                    writer.write(optional + " string " + argumentName + " = " + Integer.toString(i) + ";");
+                }
+                String required = entry.getValue().isRequired() ? "*required* " : "";
+                writer.write(" // " + required + entry.getValue().getDescription() + "\n");
+                i++;
+            }
+            writer.write( "  map<string, string> configuration = " + i + ";\n");
+            writer.write("}\n");
+            writer.write("\n");
+            writer.write("message Response {\n");
+            writer.write("  string message = 1;\n");
+            writer.write("}\n");
+        }
     }
 
     // https://www.geeksforgeeks.org/convert-camel-case-string-to-snake-case-in-java/
@@ -120,6 +133,11 @@ public class GenerateProtobufCommandStep extends AbstractCommandStep {
             }
         }
         return result;
+    }
+
+    private CommandDefinition getCommand(String targetCommand) {
+        final CommandFactory commandFactory = getCurrentScope().getSingleton(CommandFactory.class);
+        return commandFactory.getCommandDefinition(targetCommand);
     }
 
     private SortedSet<CommandDefinition> getCommands() {
